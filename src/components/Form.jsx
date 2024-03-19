fbf8d3195404ab2e755fe2ba61b70335b669bc9c
AlinaB <bernardezalina@gmail.com>
2024-03-19 19:40:07 +0100

    challenge

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,33 +1,49 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import getPoke from "../api/getPoke";
+import styles from '../components/Form.module.css'
 
 function Form () {
 const [search, setSearch] = useState('');
 const [result, setResult] = useState();
+const [error, setError] = useState('');
 
-useEffect(() => {
-    console.log(result)
-}, [result])
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(search)
     }
 
     const renderData = async() => {
         let response = await getPoke(search)
-        setResult(response)
+        if(!response) {
+            setError('No Pokemon found!')
+            setResult()
+        }
+        else {
+            setResult(response)
+            setError('')
+        }
     }
 
     return (
         <> 
             <h2>Search Pokemon</h2>
-            <form onSubmit={handleSubmit}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <label htmlFor="search">Enter a name</label>
                 <input id="search" name="search" value={search} onChange={(e) => setSearch(e.target.value)}/>
                 <button type='submit' onClick={renderData}>Search</button>
             </form>
-            {/* {result && (<p>{result}</p>)} */}
+            { error && (
+                <div className={styles.card}>
+                    <p>{error}</p>
+                </div>)
+            }
+            { result && (
+                <div className={styles.card}>
+                    <p><b>Pokemon:</b> {result.name}</p>
+                    <img src={result.sprites.front_default} />
+                    <p><b>Order:</b> {result.order}</p>
+                </div>)
+            }
         </>
     )
 }
